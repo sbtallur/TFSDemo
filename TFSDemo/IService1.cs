308b7d17903b6a6ff0eed086180aa4a7f37d501e
Nirav Bhavsar <Nirav.Bhavsar@laseruk.com>
2016-12-22 14:09:44 +0000

    removed my changes

--- TFSDemo/IService1.cs
+++ TFSDemo/IService1.cs
@@ -11,8 +11,7 @@ namespace TFSDemo
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IService1
-    {
-        //Changes for Git test
+    {        
         [OperationContract]
         string GetData(int value);
 
