d48dd47862d93fe24fb5d7161172c444aa657078
stallur <shreedhar.tallur@laseruk.com>
2017-01-05 14:00:33 +0000

    test 1

--- TFSDemo/IService1.cs
+++ TFSDemo/IService1.cs
@@ -14,7 +14,7 @@ namespace TFSDemo
     {        
         [OperationContract]
         string GetData(int value);
-        //test
+        //test feature1
         [OperationContract]
         CompositeType GetDataUsingDataContract(CompositeType composite);
 
